a9902e449e5746c30a23e77929ae08df31875b75
Zaheer <szaheer3011@gmail.com>
2026-07-18 11:29:33 +0530

    added frontend url in server.js

--- server/server.js
+++ server/server.js
@@ -7,7 +7,11 @@ require("./db/mongoose");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://zaheerportfoliopage.netlify.app/",
+  }),
+);
 app.use(express.json());
 app.use(router);
 
